63d8613a1cb3e555d7ae673fcb3290cc36f5da7a
Jesús Eduardo Pacheco <jpacheco@bluepeople.com>
2026-09-04 16:32:57 -0600

    Update calculations.js

--- calculations.js
+++ calculations.js
@@ -1,5 +1,11 @@
-function calc(a, b) {
-    var resultado = a + b;
-    var noUsada = 123;
+function addition(a, b) {
+    var result = a + b;
+    var unused = 123;
     return a + b;
+}
+
+function substraction(a,b) {
+    var result = a - b;
+    var unused = 456;
+    return a - b;
 }
